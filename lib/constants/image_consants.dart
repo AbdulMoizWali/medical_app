8bad513621a2f216e4c671cfc1b2aed47bddac86
AbdulMoizWali <85098925+AbdulMoizWali@users.noreply.github.com>
2024-07-27 13:35:50 +0000

    Walkhthrough Screen completed

--- lib/constants/image_consants.dart
+++ lib/constants/image_consants.dart
@@ -1,4 +1,5 @@
 class ImageConsants {
   static const String backgroundImage = 'assets/images/logo_background.jpg';
   static const String logoImage = 'assets/images/logo.jpg';
+  static const String walkthroughImage = 'assets/images/walkthrough.jpg';
 }
--- lib/screens/walkthrough_screen.dart
+++ lib/screens/walkthrough_screen.dart
@@ -1,20 +1,81 @@
-    import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:medical_app/constants/image_consants.dart';
+import 'package:medical_app/helpers/gap.dart';
+import 'package:medical_app/theme/theme_colors.dart';
 
-    class WalkthroughScreen extends StatefulWidget {
-      const WalkthroughScreen({super.key});
+class WalkthroughScreen extends StatefulWidget {
+  const WalkthroughScreen({super.key});
 
-      @override
-      State<WalkthroughScreen> createState() => _WalkthroughScreenState();
-    }
+  @override
+  State<WalkthroughScreen> createState() => _WalkthroughScreenState();
+}
 
-    class _WalkthroughScreenState extends State<WalkthroughScreen> {
-      @override
-      Widget build(BuildContext context) {
-        return const Scaffold(
-          body: Center(
-            child: Text('WalkthroughScreen'),
-          ),
-        );
-      }
-    }
-    
+class _WalkthroughScreenState extends State<WalkthroughScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              // color: Colors.amber,
+              child: Image.asset(ImageConsants.walkthroughImage),
+            ),
+            vGap(5),
+            const Text(
+              'View and buy',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            vGap(5),
+            const Text(
+              'Medicine online',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            vGap(5),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 70.0),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac porttitor purus.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    // fontWeight: FontWeight.bold,
+                    // color: Colors.black,
+                    ),
+              ),
+            ),
+            const Spacer(),
+            const Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Skip'),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      FontAwesome.circle_dot,
+                      size: 15,
+                    ),
+                  ],
+                ),
+                Text('Next'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
